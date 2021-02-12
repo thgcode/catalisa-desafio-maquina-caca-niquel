3d68dfe51f16257a50b938d709606f2475ba761a
Thiago Seus <thiago.seus@yahoo.com.br>
2021-02-12 14:28:56 -0300

    Adicionar classe dos níveis

---
+++ src/br/com/zup/Nivel.java
@@ -0,0 +1,42 @@
+package br.com.zup;
+
+import java.util.*;
+
+/**
+ * Representa um nível de jogo com suas possibilidades.
+ *
+ */
+public class Nivel {
+    private List <Opcao> possibilidades;
+    private Random sorteador;
+
+    /**
+     * Constroi o nível com as possibilidades especificadas.
+     *
+     * @param possibilidades uma lista com as possibilidades que podem ser escolhidas pela máquina
+     */
+    public Nivel(List <Opcao> possibilidades) {
+        sorteador = new Random();
+        this.possibilidades = possibilidades;
+    }
+
+    /**
+     * Sorteia uma lista com possibilidades de opção
+     *
+     * @return uma lista com as possibilidades escolhidas
+     */
+    public List <Opcao> sortearOpcoes() {
+        List <Opcao> opcoesEscolhidas = new ArrayList <>();
+
+        for (int i = 0; i < possibilidades.size(); i ++) {
+            Opcao opcao = possibilidades.get(sorteador.nextInt(possibilidades.size()));
+            opcoesEscolhidas.add(opcao);
+        }
+
+        return opcoesEscolhidas;
+    }
+
+    public List <Opcao> getPossibilidades() {
+        return possibilidades;
+    }
+}
