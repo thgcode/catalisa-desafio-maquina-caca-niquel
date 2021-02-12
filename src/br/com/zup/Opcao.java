e414aabfa8f484db860e2685aac9586df6f111b8
Thiago Seus <thiago.seus@yahoo.com.br>
2021-02-12 13:36:52 -0300

    Adicionar primeira versão do código com o enum das opções possíveis no jogo criada.

---
+++ src/br/com/zup/Opcao.java
@@ -0,0 +1,30 @@
+package br.com.zup;
+
+/**
+ * Armazena cada opção de possibilidade de pontuação do caça níquel
+ *
+ */
+public enum Opcao {
+    BANANA(10),
+    MORANGO(20),
+    ESTRELA(40);
+    public int quantidadeDePontos;
+
+    /**
+     * Constrói uma opção com a quantidade de pontos especificada.
+     *
+     * @param quantidadeDePontos a quantidade de pontos que essa opção vai dar ao jogador
+     */
+    Opcao(int quantidadeDePontos) {
+        this.quantidadeDePontos = quantidadeDePontos;
+    }
+
+    /**
+     * Pega o valor da pontuação dessa opção
+     *
+     * @return a pontuação que essa opção dá ao jogador
+     */
+    public int getQuantidadeDePontos() {
+        return quantidadeDePontos;
+    }
+}
