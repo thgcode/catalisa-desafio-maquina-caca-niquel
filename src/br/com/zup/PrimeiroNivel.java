c830e198b67a20af8a6db5de2d84e8c6c81ecb69
Thiago Seus <thiago.seus@yahoo.com.br>
2021-02-12 14:44:52 -0300

    Criar a classe do primeiro nível

---
+++ src/br/com/zup/PrimeiroNivel.java
@@ -0,0 +1,9 @@
+package br.com.zup;
+
+import java.util.Arrays;
+
+public class PrimeiroNivel extends Nivel {
+    public PrimeiroNivel() {
+        super(Arrays.asList(Opcao.BANANA, Opcao.MORANGO, Opcao.ESTRELA));
+    }
+}
